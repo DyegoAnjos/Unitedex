79588bcac902e5df3b60f8e552fad372dd9b922a
Dyego Anjos <107328787+DyegoAnjos@users.noreply.github.com>
2022-11-08 21:11:37 -0300

    Elenco com js

--- js/elenco-script.js
+++ js/elenco-script.js
@@ -4,98 +4,59 @@ const defensor = document.getElementById('defensor')
 const velocista = document.getElementById('velocista')
 const suporte = document.getElementById('suporte')
 
-const iconesVersatil = [
-    {src:"../imgs/iconePokemon/Aegislash.png", alt:"Aegislash", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Azumarill.png", alt:"Azumarill", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Buzzwole.png", alt:"Buzzwole", clase:"poke-icon", valor:0},
+const icones = [
+    {nome:"Aegislash", valor:0, id:"V"},
+    {nome:"Azumarill", valor:0, id:"V"},
+    {nome:"Buzzwole", valor:0, id:"V"},
+    {nome:"Cinderace", valor:0, id:"A"},
+    {nome:"Cramorant", valor:0, id:"A" },
+    {nome:"Decidueye", valor:0, id:"A"},
+    {nome:"Blastoise", valor:0, id:"D"},
+    {nome:"Crustle", valor:0, id:"D"},
+    {nome:"Greedent", valor:0, id:"D"},
+    {nome:"Absol", valor:0, id:"Vl"},
+    {nome:"Dodrio", valor:0, id:"Vl"},
+    {nome:"Gengar", valor:0, id:"Vl"},
+    {nome:"Blissey", valor:0, id:"S"},
+    {nome:"Clefable", valor:0, id:"S"},
+    {nome:"Eldegoss", valor:0, id:"S"},
 ]
-
-const iconesAtacante = [
-    {src:"../imgs/iconePokemon/Cinderace.png", alt:"Cinderace", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Cramorant.png", alt:"Cramorant", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Decidueye.png", alt:"Decidueye", clase:"poke-icon", valor:0},
-]
-
-const iconesDefensor = [
-    {src:"../imgs/iconePokemon/Blastoise.png", alt:"Blastoise", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Crustle.png", alt:"Crustle", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Greedent.png", alt:"Greedent", clase:"poke-icon", valor:0},
-]
-
-const iconesVelocista = [
-    {src:"../imgs/iconePokemon/Absol.png", alt:"Absol", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Dodrio.png", alt:"Dodrio", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Gengar.png", alt:"Gengar", clase:"poke-icon", valor:0},
-]
-
-const iconesSuporte = [
-    {src:"../imgs/iconePokemon/Blissey.png", alt:"Blissey", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Clefable.png", alt:"Clefable", clase:"poke-icon", valor:0},
-    {src:"../imgs/iconePokemon/Eldegoss.png", alt:"Eldegoss", clase:"poke-icon", valor:0},
-]
-
 
 function caixas(){
-    for(var i=0;i<=iconesVersatil.length-1;i++){
+    for(var i=0;i<=icones.length-1;i++){
         const pokeicone = document.createElement("img")
-        pokeicone.setAttribute("src",iconesVersatil[i].src)
-        pokeicone.setAttribute("alt",iconesVersatil[i].alt)
-        pokeicone.setAttribute("class",iconesVersatil[i].clase)
+        pokeicone.setAttribute("src","../imgs/iconePokemon/"+icones[i].nome+".png")
+        pokeicone.setAttribute("alt",icones[i].nome)
+        pokeicone.setAttribute("class","poke-icon")
 
-        iconesVersatil[i].valor=i
-        pokeicone.setAttribute("onclik", `pokeinfo(${iconesVersatil[i].valor})`)
-        console.log(iconesVersatil[i].valor)
-        versatil.appendChild(pokeicone)
+        icones[i].valor=i
+        pokeicone.setAttribute("onclick", `pokeinfo(${icones[i].valor})`)
+
+        switch (icones[i].id){
+            case "V":
+                versatil.appendChild(pokeicone)
+            break;
+            case "A":
+                atacante.appendChild(pokeicone)
+            break;
+            case "D":
+                defensor.appendChild(pokeicone)
+            break;
+            case "Vl":
+                velocista.appendChild(pokeicone)
+            break;
+            case "S":
+                suporte.appendChild(pokeicone)
+            break;
+        }
     }
+}
 
-    for(var i=0;i<=iconesAtacante.length-1;i++){
-        const pokeicone = document.createElement("img")
-        pokeicone.setAttribute("src",iconesAtacante[i].src)
-        pokeicone.setAttribute("alt",iconesAtacante[i].alt)
-        pokeicone.setAttribute("class",iconesAtacante[i].clase)
+const fichas = [
+    {id: 0, nome:"Indefeso"}
+]
 
-        iconesAtacante[i].valor=iconesAtacante[i].valor+i+iconesVersatil.length
-        pokeicone.setAttribute("onclik", `pokeinfo(${iconesAtacante[i].valor})`)
-        console.log(iconesAtacante[i].valor)
-        atacante.appendChild(pokeicone)
-    }
 
-    for(var i=0;i<=iconesDefensor.length-1;i++){
-        const pokeicone = document.createElement("img")
-        pokeicone.setAttribute("src",iconesDefensor[i].src)
-        pokeicone.setAttribute("alt",iconesDefensor[i].alt)
-        pokeicone.setAttribute("class",iconesDefensor[i].clase)
+function pokeinfo(id){
 
-        iconesDefensor[i].valor=iconesDefensor[i].valor+i+iconesAtacante.length
-
-        pokeicone.setAttribute("onclik", `pokeinfo(${iconesDefensor[i].valor})`)
-        console.log(iconesDefensor[i].valor)
-        defensor.appendChild(pokeicone)
-    }
-
-    for(var i=0;i<=iconesVelocista.length-1;i++){
-        const pokeicone = document.createElement("img")
-        pokeicone.setAttribute("src",iconesVelocista[i].src)
-        pokeicone.setAttribute("alt",iconesVelocista[i].alt)
-        pokeicone.setAttribute("class",iconesVelocista[i].clase)
-
-        iconesVelocista[i].valor=iconesVelocista[i].valor+i+iconesDefensor.length
-
-        pokeicone.setAttribute("onclik", `pokeinfo(${iconesVelocista[i].valor})`)
-        console.log(iconesVelocista[i].valor)
-
-        velocista.appendChild(pokeicone)
-    }
-
-    for(var i=0;i<=iconesSuporte.length-1;i++){
-        const pokeicone = document.createElement("img")
-        pokeicone.setAttribute("src",iconesSuporte[i].src)
-        pokeicone.setAttribute("alt",iconesSuporte[i].alt)
-        pokeicone.setAttribute("class",iconesSuporte[i].clase)
-        iconesSuporte[i].valor=iconesSuporte[i].valor+i+iconesVelocista.length
-        
-        pokeicone.setAttribute("onclik", `pokeinfo(${iconesSuporte[i].valor})`)
-        console.log(iconesSuporte[i].valor)
-        suporte.appendChild(pokeicone)
-    }
 }
